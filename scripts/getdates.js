22c1543fbe06572c1503cc099dbed793bdc6e6bc
Simire Simeon <simiresimeon@gmail.com>
2026-05-24 02:00:16 +0100

    Add dynamic year and last modified date to footer; include getdates.js script

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
